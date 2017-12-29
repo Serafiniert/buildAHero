348a0dadbc82c6c24cabf60f6120a617511b0eef
NinaD <s0556156@htw-berlin.de>
2017-12-29 18:04:01 +0100

    erster Entwurf für Start-Screen

---
+++ app/src/main/java/de/build_a_hero/app/Main2Activity.kt
@@ -0,0 +1,15 @@
+package de.build_a_hero.app
+
+import android.os.Bundle
+import android.app.Activity
+
+import kotlinx.android.synthetic.main.activity_main2.*
+
+class Main2Activity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+
+}
