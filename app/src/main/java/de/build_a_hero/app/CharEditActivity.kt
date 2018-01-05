43df66db962320071ff5238a8cba2031cc38d469
Tim <s0556736@htw-berlin.de>
2018-01-05 15:35:35 +0100

    Charaktererstellung/-bearbeitung erstellt + Kontakt bei Settings hinzugefügt

---
+++ app/src/main/java/de/build_a_hero/app/CharEditActivity.kt
@@ -0,0 +1,12 @@
+package de.build_a_hero.app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CharEditActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_char_edit)
+    }
+}
