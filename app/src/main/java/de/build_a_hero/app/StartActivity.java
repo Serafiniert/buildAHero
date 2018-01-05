462652ab8065a4fc7bb3517fab29b882bdc523b0
Serafiniert <stoyloff@designmail.net>
2018-01-05 17:43:48 +0100

    Fixed constrains Renamed .xml files, again

--- app/src/main/java/de/build_a_hero/app/StartActivity.java
+++ app/src/main/java/de/build_a_hero/app/StartActivity.java
@@ -14,7 +14,7 @@ public class StartActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
+        setContentView(R.layout.fragment_start);
 
         configureNextButton();
 
--- app/src/main/java/de/build_a_hero/app/StartActivityFragment.java
+++ app/src/main/java/de/build_a_hero/app/StartActivityFragment.java
@@ -15,6 +15,6 @@ public class StartActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        return inflater.inflate(R.layout.activity_start, container, false);
     }
 }
