d4668a0af0ea70e32317f2864ec4b5e9d41da68e
NinaD <s0556156@htw-berlin.de>
2018-01-05 17:58:47 +0100

    no message

--- app/src/main/java/de/build_a_hero/app/StartActivity.java
+++ app/src/main/java/de/build_a_hero/app/StartActivity.java
@@ -4,8 +4,6 @@ package de.build_a_hero.app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -33,20 +31,19 @@ public class StartActivity extends AppCompatActivity {
         );
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
     public void buttonOnClick(View v) {
         Button button = (Button) v;
         startActivity(new Intent(getApplicationContext(), MenuActivity.class));
     }
 
-    @Override
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }*/
+
+    /*@Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -59,5 +56,5 @@ public class StartActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
