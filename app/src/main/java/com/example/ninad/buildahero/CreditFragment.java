847c373fd8d76dba1080c1d627a064f9f43b7e63
Tim <s0556736@htw-berlin.de>
2018-01-15 21:00:36 +0100

    Fragment

---
+++ app/src/main/java/com/example/ninad/buildahero/CreditFragment.java
@@ -0,0 +1,23 @@
+package com.example.ninad.buildahero;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import de.build_a_hero.app.R;
+
+
+public class CreditFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_credit, container, false);
+    }
+
+
+}
