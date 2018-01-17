05822932e2d4a3043c2b8df60245a509043ae5c4
Finn Christians <finnchristians@gmail.com>
2018-01-17 12:17:08 +0100

    fixed internet issue: now empty Spinner for charNames if no internet is available begin of kotlin converting

---
+++ app/src/main/java/de/build_a_hero/app/CSVFile.kt
@@ -0,0 +1,78 @@
+package de.build_a_hero.app
+
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.util.ArrayList
+import java.util.Collections
+
+class CSVFile(private val inputStream: InputStream) {
+    private var female: MutableList<String>? = null
+    private var male: MutableList<String>? = null
+    private var allNames: MutableList<String>? = null
+
+    fun read() {
+        val reader = BufferedReader(InputStreamReader(inputStream))
+        female = ArrayList()
+        male = ArrayList()
+        allNames = ArrayList()
+
+        try {
+            var csvLine: String
+            csvLine = reader.readLine()
+
+            while (/*(csvLine = reader.readLine())*/ csvLine != null) {
+                val row = csvLine.split(";".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+                if (row[2].equals("w", ignoreCase = true)) {
+                    female!!.add(row[0])
+                } else if (row[2].equals("m", ignoreCase = true)) {
+                    male!!.add(row[0])
+                }
+                csvLine = reader.readLine()
+
+
+            }
+            allNames!!.addAll(female!!)
+            allNames!!.addAll(male!!)
+            Collections.sort(allNames!!)
+            Collections.sort(female!!)
+            Collections.sort(male!!)
+
+        } catch (ex: IOException) {
+            throw RuntimeException("Error in reading CSV file: " + ex)
+        } finally {
+            try {
+                inputStream.close()
+            } catch (e: IOException) {
+                throw RuntimeException("Error while closing input stream: " + e)
+            }
+
+        }
+    }
+
+
+    fun getFemale(): List<String>? {
+        return female
+    }
+
+    fun setFemale(female: MutableList<String>) {
+        this.female = female
+    }
+
+    fun getMale(): List<String>? {
+        return male
+    }
+
+    fun setMale(male: MutableList<String>) {
+        this.male = male
+    }
+
+    fun getAllNames(): List<String>? {
+        return allNames
+    }
+
+    fun setAllNames(allNames: MutableList<String>) {
+        this.allNames = allNames
+    }
+}
